9a9ec40912d7ffc40b0117ce94ac5af261251db4
Caio <thehaseol@gmail.com>
2021-02-24 15:55:21 -0300

    test de codigo JS .

---
+++ bloco_4/scripts.js
@@ -0,0 +1,13 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log( typeof patientId);
+console.log( typeof isEnrolled);
+console.log( typeof patientInfo.firstName);
+console.log( typeof patientInfo.lastName);
+console.log( typeof patientEmail);
