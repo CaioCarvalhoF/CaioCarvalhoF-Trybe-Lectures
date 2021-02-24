a5bf21697367aea6ba0585e8a9b0a76557e59876
Caio <thehaseol@gmail.com>
2021-02-24 18:45:06 -0300

    Exercicio finalizado .

---
+++ bloco_4/Switch/case.js
@@ -0,0 +1,21 @@
+let estado = 'A';
+
+switch (estado) {
+    case 'aprovada':
+        console.log('aprovada');
+        break;
+
+    case 'reprovada':
+        console.log('reprovada');
+        break;
+
+
+    case 'lista':
+        console.log('lista');
+        break;
+
+
+    default:
+        console.log('não se aplica')
+
+}
