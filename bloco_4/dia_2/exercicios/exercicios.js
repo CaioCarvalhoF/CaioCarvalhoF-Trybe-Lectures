0344a24d453f4a6b047dfcc67d22f8883f8dc636
Caio <thehaseol@gmail.com>
2021-02-25 20:36:17 -0300

    exercicios finalizados

--- bloco_4/dia_2/exercicios/exercicios.js
+++ bloco_4/dia_2/exercicios/exercicios.js
@@ -15,7 +15,7 @@
 
 
 //3- Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
-
+/*
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let soma = 0;
@@ -26,10 +26,11 @@ for(let iterador = 0; iterador < numbers.length; iterador +=1) {
 let media = soma / numbers.length
 
 console.log(media);
+*/
 
 //4- Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
-
+/*
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let soma = 0;
@@ -41,13 +42,13 @@ let media = soma / numbers.length
 
 console.log(media);
 
-
+*/
 
 
 
 
 // 5 - Utilizando for , descubra qual o maior valor contido no array e imprima-o;
-
+/*
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 maiorNumero = 0;
 
@@ -58,34 +59,63 @@ for(let iterador = 0; iterador < numbers.length; iterador +=1) {
 
 }
 console.log(maiorNumero);
+*/
 
+//6 - Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+/*
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* serve para dividir todos os numeros do array
-  let soma = 0;
-let media = 0
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let odd = 0;
 
 for(let iterador = 0; iterador < numbers.length; iterador +=1) {
-  soma+= numbers[iterador];
-  media += soma / numbers.length;
-  console.log(media);
+  if (numbers[iterador] % 2 == 0) {
+      odd += 1;
+  }
+  else {
+      console.log("nenhum valor ímpar encontrado");
+  }
+
 }
-*/
+console.log(odd);
+
+ */
+
+//7 - Utilizando for , descubra qual o menor valor contido no array e imprima-o;
+/**
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+menorNumero = 5;
+
+for(let iterador = 0; iterador < numbers.length; iterador +=1) {
+  if (numbers[iterador] < menorNumero) {
+    menorNumero= numbers[iterador]
+  }
+
+}
+console.log(menorNumero);
+
+ */
+
+//8 - Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
+
+
+
+let numbers = [];
+
+for (let i = 1; i <= 25; i += 1) {
+  numbers.push(i);
+}
+
+console.log(numbers);
+
+
+
+// 9 - Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+
+
+
+/* serve para dividir todos os numeros do array*/
+
+for (let i = 0; i < numbers.length; i += 1) {
+    console.log(numbers[i] / 2);
+  };
