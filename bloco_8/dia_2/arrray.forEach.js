140a37db64aed73f1a73c682515ab9b6069ed675
Caio <thehaseol@gmail.com>
2021-03-30 18:25:25 -0300

    eexercicios de 01 a 03

---
+++ bloco_8/dia_2/arrray.forEach.js
@@ -0,0 +1,29 @@
+//Estrutura das HOF:
+
+
+const arrayOfValues = ['josé', 50, 0.25, { comida: 'Chocolate' }];
+arrayOfValues.forEach((element) => {
+  console.log('Cada elemento do array:', element);
+});
+
+// Cada elemento do array: josé
+// Cada elemento do array: 50
+// Cada elemento do array: 0.25
+// Cada elemento do array: { comida: 'Chocolate' }
+
+/*arrayOfValues.forEach((element) => {
+    console.log('Cada elemento do array:', element);
+});
+*/
+
+
+
+
+const names = ['Bianca', 'Camila', 'Fernando', 'Ana Roberta'];
+
+const convertToUpperCase = (name, index) => {
+  names[index] = name.toUpperCase();
+};
+
+names.forEach(convertToUpperCase);
+console.log(names); // [ 'BIANCA', 'CAMILA', 'FERNANDO', 'ANA ROBERTA' ]
